cb2013108d954ad19b6eb023f670d162a2327bf7
rvmartin1 <rvmartin1@gmail.com>
2023-10-15 20:27:45 +0400

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-
+import { useState } from "react"
 
 function App() {
-   return "Bonjour les gens"
+   const [count,setCount]=useState(0);
+   console.log("render")
+   const [person, setPerson]=useState({
+      lastName:"John",
+      firstName:"Doe",
+      age:18
+   })
+
+   const incrementCount=()=>{
+      setCount(count+1);
+   }
+
+   const incrementAge=()=>{
+      setPerson({...person,age:person.age+1});
+   }
+
+   return <>
+   <p>compteur : {count}</p>
+   <button onClick={incrementCount}>Incrementer compteur </button>
+   <p>Nom : {person.firstName}, age : {person.age}</p>
+   <button onClick={incrementAge}>Incrementer Age </button>
+   </>
 }
 
 export default App
